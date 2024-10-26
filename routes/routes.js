a628f9dfb02a7dfcce8cc49bb8b29b4c56f2d7a2
slayer182002 <leninpantoja673@gmail.com>
2024-10-26 13:17:47 -0600

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,1 +1,16 @@
 //Importa rutas
+import { Router } from "express";
+import empleadoRoutes from "./empleadoRoutes.js";
+import departamentoRoutes from "./departamentoRoutes.js";
+import encargadoRoutes from "./encargadoRoutes.js";
+import areaRoutes from "./areaRoutes.js";
+
+const router = Router();
+
+// Definir las rutas para cada entidad
+router.use("/empleados", empleadoRoutes);
+router.use("/departamentos", departamentoRoutes);
+router.use("/encargados", encargadoRoutes);
+router.use("/areas", areaRoutes);
+
+export default router;
