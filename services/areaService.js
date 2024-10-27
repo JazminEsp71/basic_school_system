818deb9475ed39b0e2fdb3b560bf4b0a80bec3af
slayer182002 <leninpantoja673@gmail.com>
2024-10-26 20:31:22 -0600

    Update areaService.js

--- services/areaService.js
+++ services/areaService.js
@@ -1,13 +1,46 @@
 import areas from "../models/areaModel.js";
 
-export default class areaService{
-    constructor(){
-        //Utiliza el array de empleados importado de la carpeta 'models'
-        this.areas = areas;  // Almacenamos los datos de 'empleadoModel'
-    }
+class AreaService {
+  constructor() {
+    this.areas = [...areas]; // Crear una copia de los datos de área
+    this.currentId = this.areas.length + 1; // Continuar el ID desde el último en el modelo
+  }
 
-    //Obtener todos los empleados almacenados
-    getAll(){
-        return this.areas;
+  create(data) {
+    const newArea = {
+      idArea: this.currentId++,
+      ...data
+    };
+    this.areas.push(newArea);
+    return newArea;
+  }
+
+  getAll() {
+    return this.areas;
+  }
+
+  getById(id) {
+    return this.areas.find(area => area.idArea === id);
+  }
+
+  update(id, changes) {
+    const index = this.areas.findIndex(area => area.idArea === id);
+    if (index === -1) {
+      throw new Error("Area not found");
     }
+    const area = this.areas[index];
+    this.areas[index] = { ...area, ...changes };
+    return this.areas[index];
+  }
+
+  delete(id) {
+    const index = this.areas.findIndex(area => area.idArea === id);
+    if (index === -1) {
+      throw new Error("Area not found");
+    }
+    this.areas.splice(index, 1);
+    return { id };
+  }
 }
+
+export default AreaService;
