b6cdeac3bf07be2c7df9f5fb043a169ed61d6f32
JazminEsp71 <jazminesp71@gmail.com>
2024-10-29 17:54:31 -0600

    2 departamentos agregados

--- models/empleadoModel.js
+++ models/empleadoModel.js
@@ -1,14 +1,14 @@
 const empleados = [
-    { numeroEmpleado: 1001, nombre: "Juan", apellido: "Pérez", edad: 30, genero: "Masculino", departamentoId: 1 },
-    { numeroEmpleado: 1002, nombre: "Ana", apellido: "Gómez", edad: 25, genero: "Femenino", departamentoId: 2 },
-    { numeroEmpleado: 1003, nombre: "Luis", apellido: "Fernández", edad: 40, genero: "Masculino", departamentoId: 3 },
-    { numeroEmpleado: 1004, nombre: "Laura", apellido: "García", edad: 35, genero: "Femenino", departamentoId: 4 },
-    { numeroEmpleado: 1005, nombre: "Pedro", apellido: "Morales", edad: 28, genero: "Masculino", departamentoId: 5 },
-    { numeroEmpleado: 1006, nombre: "Sofía", apellido: "López", edad: 22, genero: "Femenino", departamentoId: 6 },
-    { numeroEmpleado: 1007, nombre: "Carlos", apellido: "Ramírez", edad: 45, genero: "Masculino", departamentoId: 7 },
-    { numeroEmpleado: 1008, nombre: "Isabel", apellido: "Méndez", edad: 27, genero: "Femenino", departamentoId: 8 },
-    { numeroEmpleado: 1009, nombre: "Alberto", apellido: "Cruz", edad: 33, genero: "Masculino", departamentoId: 9 },
-    { numeroEmpleado: 1010, nombre: "Rosa", apellido: "Hernández", edad: 29, genero: "Femenino", departamentoId: 10 }
+    { numeroEmpleado: 1001, nombre: "Juan", apellido: "Pérez", edad: 30, genero: "Masculino", departamentoId: 1 , departamentoId2: 2, departamentoId3: 7},
+    { numeroEmpleado: 1002, nombre: "Ana", apellido: "Gómez", edad: 25, genero: "Femenino", departamentoId: 2, departamentoId2: 6, departamentoId3: 1},
+    { numeroEmpleado: 1003, nombre: "Luis", apellido: "Fernández", edad: 40, genero: "Masculino", departamentoId: 3 , departamentoId2: 3, departamentoId3: 2},
+    { numeroEmpleado: 1004, nombre: "Laura", apellido: "García", edad: 35, genero: "Femenino", departamentoId: 4 , departamentoId2: 1, departamentoId3: 3},
+    { numeroEmpleado: 1005, nombre: "Pedro", apellido: "Morales", edad: 28, genero: "Masculino", departamentoId: 5 , departamentoId2: 7, departamentoId3: 2},
+    { numeroEmpleado: 1006, nombre: "Sofía", apellido: "López", edad: 22, genero: "Femenino", departamentoId: 6 , departamentoId2: 2,  departamentoId3: 4},
+    { numeroEmpleado: 1007, nombre: "Carlos", apellido: "Ramírez", edad: 45, genero: "Masculino", departamentoId: 7 , departamentoId2: 4, departamentoId3: 2},
+    { numeroEmpleado: 1008, nombre: "Isabel", apellido: "Méndez", edad: 27, genero: "Femenino", departamentoId: 8 , departamentoId2: 1, departamentoId3: 2},
+    { numeroEmpleado: 1009, nombre: "Alberto", apellido: "Cruz", edad: 33, genero: "Masculino", departamentoId: 9 , departamentoId2: 3, departamentoId3: 2},
+    { numeroEmpleado: 1010, nombre: "Rosa", apellido: "Hernández", edad: 29, genero: "Femenino", departamentoId: 10 , departamentoId2: 1, departamentoId3: 2}
   ];
 
   export default empleados;
