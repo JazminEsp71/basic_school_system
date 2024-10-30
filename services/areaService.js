2dd718ff12112ea86cb0e36244f6d44a466bef1f
slayer182002 <leninpantoja673@gmail.com>
2024-10-29 20:35:04 -0600

    Update areaService.js

--- services/areaService.js
+++ services/areaService.js
@@ -6,24 +6,41 @@ class AreaService {
     this.currentId = this.areas.length + 1; // Continuar el ID desde el último en el modelo
   }
 
-  create(data) {
-    const newArea = {
-      idArea: this.currentId++,
-      ...data
-    };
-    this.areas.push(newArea);
-    return newArea;
-  }
+ // Validar si el edificio existe
+ validateEdificio(edificio) {
+  return this.areas.some((area) => area.edificio === edificio)
+    ? { success: true }
+    : {
+        success: false,
+        message: `El edificio ${edificio} no existe. Verifica los datos`,
+      };
+}
 
+// Crear una nueva área
+create(data) {
+  // Validar edificio
+  const validateEdificioResult = this.validateEdificio(data.edificio);
+  if (!validateEdificioResult.success) return validateEdificioResult;
+
+  const newArea = {
+    idArea: this.currentId++,
+    ...data
+  };
+
+  this.areas.push(newArea);
+  return newArea;
+}
+
+//Obtener todos los datos de Area
   getAll() {
     return this.areas;
   }
-
+//Obtenerlo por el ID dado
   getById(id) {
     return this.areas.find(area => area.idArea === id);
   }
-
-  update(id, changes){//parametros necesarios {
+//Modificar un area
+  update(id, changes){//parametros necesarios 
     const index = this.areas.findIndex(area => area.idArea === id);
     if (index === -1) {
       throw new Error("Area not found");
@@ -32,7 +49,7 @@ class AreaService {
     this.areas[index] = { ...area, ...changes };
     return this.areas[index];
   }
-
+//Eliminar un Area
   delete(id) {
     const index = this.areas.findIndex(area => area.idArea === id);
     if (index === -1) {
