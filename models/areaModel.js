05c09ca4a145c14c1ac8b51133e4d83e64a039ae
slayer182002 <leninpantoja673@gmail.com>
2024-10-29 20:40:58 -0600

    Update areaModel.js

--- models/areaModel.js
+++ models/areaModel.js
@@ -1,14 +1,14 @@
 const areas = [
-    { idArea: 1, nombre: "Comercial", edificio: "Edificio A" },
-    { idArea: 2, nombre: "Tecnología", edificio: "Edificio B" },
-    { idArea: 3, nombre: "Finanzas", edificio: "Edificio C" },
-    { idArea: 4, nombre: "Recursos Humanos", edificio: "Edificio D" },
-    { idArea: 5, nombre: "Legal", edificio: "Edificio E" },
-    { idArea: 6, nombre: "Logística", edificio: "Edificio F" },
-    { idArea: 7, nombre: "Producción", edificio: "Edificio G" },
-    { idArea: 8, nombre: "Marketing", edificio: "Edificio H" },
-    { idArea: 9, nombre: "Calidad", edificio: "Edificio I" },
-    { idArea: 10, nombre: "Compras", edificio: "Edificio J" }
+    { idArea: 1, nombre: "Comercial", edificio: "A" },
+    { idArea: 2, nombre: "Tecnología", edificio: "B" },
+    { idArea: 3, nombre: "Finanzas", edificio: "C" },
+    { idArea: 4, nombre: "Recursos Humanos", edificio: "D" },
+    { idArea: 5, nombre: "Legal", edificio: "E" },
+    { idArea: 6, nombre: "Logística", edificio: "A" },
+    { idArea: 7, nombre: "Producción", edificio: "B" },
+    { idArea: 8, nombre: "Marketing", edificio: "C" },
+    { idArea: 9, nombre: "Calidad", edificio: "D" },
+    { idArea: 10, nombre: "Compras", edificio: "E" }
   ];
 
 export default areas;
