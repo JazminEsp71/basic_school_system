3fd7bf3741b21ad4f20ecf664bcfc155eec5beae
Jazmin Esparza <jazminesp71@gmail.com>
2024-12-04 16:47:12 -0600

    Clase areaService actualizada

--- services/areaService.js
+++ services/areaService.js
@@ -1,62 +1,45 @@
 import areas from "../models/areaModel.js";
+import departamentos from "../models/departamentoModel.js";
 
 class AreaService {
-  constructor() {
-    this.areas = [...areas]; // Crear una copia de los datos de área
-    this.currentId = this.areas.length + 1; // Continuar el ID desde el último en el modelo
+  async create(data){
+    const newArea = new areas(data);
+    await newArea.save();
+    return newArea
   }
-
- // Validar si el edificio existe
- validateEdificio(edificio) {
-  return this.areas.some((area) => area.edificio === edificio)
-    ? { success: true }
-    : {
-        success: false,
-        message: `El edificio ${edificio} no existe. Verifica los datos`,
-      };
-}
-
-// Crear una nueva área
-create(data) {
-  // Validar edificio
-  const validateEdificioResult = this.validateEdificio(data.edificio);
-  if (!validateEdificioResult.success) return validateEdificioResult;
-
-  const newArea = {
-    idArea: this.currentId++,
-    ...data
-  };
-
-  this.areas.push(newArea);
-  return newArea;
-}
-
 //Obtener todos los datos de Area
-  getAll() {
-    return this.areas;
+  async getAll() {
+    return await areas.find();
   }
 //Obtenerlo por el ID dado
-  getById(id) {
-    return this.areas.find(area => area.idArea === id);
+  async getById(id) {
+    const area = await areas.findById(id);
+    if(!area){
+      throw new Error("Area no encontrada, revisa nuevamente.");
+    }return area;
   }
 //Modificar un area
-  update(id, changes){//parametros necesarios 
-    const index = this.areas.findIndex(area => area.idArea === id);
-    if (index === -1) {
-      throw new Error("Area not found");
-    }
-    const area = this.areas[index];
-    this.areas[index] = { ...area, ...changes };
-    return this.areas[index];
+  async update(id, changes){//parametros necesarios 
+    const updateArea = await areas.findByIdAnUpdate(id, changes, {
+      new: true,
+      runValidators: true,
+    });
+    if (!updateArea) {
+      throw new Error("Area no encontrada, revisa nuevamente.");
+    }return updateArea;
   }
 //Eliminar un Area
-  delete(id) {
-    const index = this.areas.findIndex(area => area.idArea === id);
-    if (index === -1) {
-      throw new Error("Area not found");
+  async delete(id) {
+    const departArea = await departamentos.find({areaId: id});
+    if (departArea.length > 0) {
+      throw new Error("Error, no es posible eliminar el area ya que esta asignada a un departamento.");
     }
-    this.areas.splice(index, 1);
-    return { id };
+
+    const deletedArea = await areas.findByIdAndDelete(id);
+    if(!deletedArea){
+      throw new Error("Area no encontrada, revisa nuevamente.")
+    }
+    return deletedArea;
   }
 }
 
